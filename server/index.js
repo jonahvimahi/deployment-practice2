bfe48505c73640c29546ea5b0e6a92be53e09c9d
jonahvimahi <jonahvimahi@gmail.com>
2021-08-26 15:11:56 -0600

    added css maybe

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 
 })
 
-// app.use('/css'), express.static(path.join(__dirname, '../styles.css'))
+app.use('/css', express.static(path.join(__dirname, '../styles.css')))
 
 const port = process.env.PORT || 4005
 
