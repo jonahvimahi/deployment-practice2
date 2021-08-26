2fa734a9c5facf4c9df53cbbcae556300f5360ee
jonahvimahi <jonahvimahi@gmail.com>
2021-08-26 14:19:19 -0600

    added css functionality

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 
 })
 
-app.use('/'), express.static(path.join(__dirname, '../index.html'))
+app.use('/css'), express.static(path.join(__dirname, '../styles.css'))
 
 const port = process.env.PORT || 4005
 
